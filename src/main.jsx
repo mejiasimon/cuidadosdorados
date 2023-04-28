eb389aa147d79d83cdadbda89906c72c6588bdd0
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-04-28 11:35:53 -0500

    footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './Home/Home'
 import {Servicios} from "./Servicios/Servicios.jsx";
+import { Footer } from "./assets/shared/Footer/Footer"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home></Home>
     <Servicios />
+    <Footer />
   </React.StrictMode>,
 )

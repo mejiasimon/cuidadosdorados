67014274d95e4404fef2f7e1373c167d6fb401c0
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-04-28 11:22:06 -0500

    forn

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './Home/Home'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import {Servicios} from "./Servicios/Servicios.jsx";
+import { Agenda } from './Agenda/Agenda';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home></Home>
     <Servicios />
+    <Agenda />
   </React.StrictMode>,
 )

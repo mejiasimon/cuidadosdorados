327f271c5239f699a1ba1c24ea2f5ae73f6dc000
saldaandres <saldaandres@hotmail.es>
2023-04-28 11:13:52 -0500

    Creamos el componente de Banner

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './Home/Home'
 import {Servicios} from "./Servicios/Servicios.jsx";
+import {Banner} from "./Banner/Banner.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home></Home>
+      <Banner />
     <Servicios />
   </React.StrictMode>,
 )

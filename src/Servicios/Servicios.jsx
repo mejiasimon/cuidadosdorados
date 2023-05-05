0214a8624611b3f237c4c9e1e0496302f93e23d9
mejiasimon <simonmejiammm@gmail.com>
2023-05-05 10:34:39 -0500

    ultimo

--- src/Servicios/Servicios.jsx
+++ src/Servicios/Servicios.jsx
@@ -12,23 +12,26 @@ wow.init()
     },[])
     return (
         <>
+     
+        
  <div className="container-servicios">
- <div className="row">
-                <div id="segunda" className="col col-md-4 wow animate__animated animate__backInLeft">
+ <center><h1>SERVICIOS</h1></center>
+ <div className="row gx-1">
+                <div id="segunda" className="col-lg-4 col-md-12 wow animate__animated animate__backInLeft">
                     <img className="imagen" src="https://firebasestorage.googleapis.com/v0/b/proyecto-vite-andres.appspot.com/o/acompa%C3%B1amiento.jpg?alt=media&token=439c6103-e1ae-4f5e-95d3-c27c22caa9b4" alt="Acompañamiento"/>
                     <div className="texto">
                         <h3>Acompañamiento a Citas y Trámites</h3>
                         <p> Si por razones laborales no puedes acompañar a tu ser querido en sus diligencias, uno de nuestros colaboradores estará para acompañarlos</p>
                     </div>
                 </div>
-                <div id="primera" className="col col-md-4 wow animate__animated animate__backInDown">
+                <div id="primera" className="col-lg-4 col-md-6 wow animate__animated animate__backInDown">
                     <img className="imagen" src="https://firebasestorage.googleapis.com/v0/b/proyecto-vite-andres.appspot.com/o/cuidado_personal.jfif?alt=media&token=5b1e181e-f618-4944-aaee-b257cb69a048" alt="Cuidado personal"/>
                     <div className="texto">
                         <h3>Cuidado Personal</h3>
                         <p> La persona encargada de ti, te brindara un aseo personal debido a tus necesidades, logrando en ti una comodidad y seguridad ante este servicio.</p>
                     </div>
                 </div>
-                <div id="tercera" className="col col-md-4 wow animate__animated animate__backInRight">
+                <div id="tercera" className="col-lg-4 col-md-6wow animate__animated animate__backInRight">
                     <img className="imagen" src="https://firebasestorage.googleapis.com/v0/b/proyecto-vite-andres.appspot.com/o/ejercicios_aerobicos.webp?alt=media&token=cc6e0db5-c6a3-478e-9194-19efba5fc581" alt="Ejercicios aerobicos"/>
                     <div className="texto">
                         <h3>Ejercicios Aerobicos</h3>
--- src/assets/shared/Menu/Menu.jsx
+++ src/assets/shared/Menu/Menu.jsx
@@ -19,12 +19,12 @@ return(
       <span className="line line3" />
     </div>
     <ul className="menu-items">
-      <li><a className={"text-black"} href="#">Home</a></li>
-      <li><a className={"text-black"} href="#">About</a></li>
-      <li><a className={"text-black"} href="#">Category</a></li>
-      <li><a className={"text-black"} href="#">Menu</a></li>
-      <li><a className={"text-black"} href="#">Testimonial</a></li>
-      <li><a className={"text-black"} href="#">Contact</a></li>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Category</a></li>
+      <li><a href="#">Menu</a></li>
+      <li><a href="#">Testimonial</a></li>
+      <li><a href="#">Contact</a></li>
     </ul>
    <div className="logo wow animate__animated animate__bounce"><img src="https://firebasestorage.googleapis.com/v0/b/cuidados-dorados.appspot.com/o/Pink%20Minimalist%20Home%20Care%20Services%20Logo.png?alt=media&token=db6e554b-0fcb-46df-a818-92f5f71714bb" alt="" /></div>
   </div>
